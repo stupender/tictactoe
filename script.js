45fd8da155c9875773d650a23873510284d97fd8
Stu Pender <stu1992@gmail.com>
2021-04-26 15:12:12 -0500

    javascript-take1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// Holds the players -- using factories
+let playerFactory = (name, selection) => {
+    let playTurn = (board, cell) => {
+        let index = board.box.findIndex(position => position === cell);
+        if (board.boardArray[index] = "") {
+            board.render();
+            return index;
+        }
+        return null
+    };
+    
+    return {
+        name,
+        mark,
+        playTurn
+    }
+};
+
 // Holds the gameboard array -- using modules
 let gameBoardModule = (() => {
     let boardArray = ["", "", "", "", "", "", "", "", ""];
@@ -48,33 +66,82 @@ let gameBoardModule = (() => {
     }
 })();
 
+
 // Controls the flow of the game -- using modules
-let gamePlay = {
+let gamePlay = (() => {
+    let playerOneName = document.querySelector("#player-1");
+    let playerTwoName = document.querySelector("#player-2");
+    let form = document.querySelector(".player-names");
+    let resetButton = document.querySelector("reset");
+    let currentPlayer;
+    let playerOne;
+    let playerTwo;
     
-};
-
-// Holds the players -- using factories
-let playerFactory = (name, selection) => {
-    let playTurn = (board, cell) => {
-        let index = board.box.findIndex(position => position === cell);
-        if (board.boardArray[index] = "") {
-            board.render();
-            return index;
-        }
-        return null
+    //Review this
+    let switchTurn = () => {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
     };
-    return {
-        name,
-        mark,
-        playTurn
-    }
-};
 
-// REVEALING MODULE PATTERN
-// let displayController = (function(){
-//     function render() {
-//     }
-//     return {
-//         render: render
-//     }
-// })()
+    let gameRound = () => {
+        let board = gameBoardModule;
+        let gameStatus = document.querySelector(".game-status");
+        if (currentPlayer.name !== "") {
+            gameStatus.textContent = `${currentPlayer.name}'s turn`
+        }
+        else {
+            gameStatus.textContent = "...";
+        }
+
+        gameBoardboardModule.gameBoard.addEventListener("click", (event) => {
+            event.preventDefault();
+            let play = currentPlayer.playTurn(board, event.target);
+            if (play !== null) {
+                board.boardArray[play] = `${currentPlayer.mark}`;
+                board.render();
+                let winStatus = board.checkWin();
+                if (winStatus == "Tie") {
+                    gameStatus.textContent = "Tie!"
+                } else if (winStatus === null) {
+                    switchTurn();
+                    gameStatus.textContent = `${currentPlayer.name}'s Turn`;
+                } else {
+                    gameStatus.textContent = `${currentPlayer.name} Wins!`;
+                    board.reset();
+                    board.render();
+                }
+            }
+        });
+    };
+
+    let gameInit = () => {
+        if (playerOneName !== "" && playerTwoName !== "") {
+            playerOne = playerFactory(playerOneName.value, "X")
+            playerTwo = playerFactory(playerTwoName.value, "O")
+            currentPlayer = playerOne;
+            gameRound();
+        }
+    };
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (playerOneName !== "" && playerTwoName !== "") {
+            gameInit();
+            form.classList.add("hidden");
+            document.querySelector(".place").classList.remove("hidden");
+        } else {
+            window.location.reload();
+        }
+    });
+
+    resetButton.addEventListener("click", () => {
+        document.querySelector(".game-status").textContent = "";
+        document.querySelector("#player1").value = "";
+        document.querySelector("#player2").value = "";
+        window.location.reload();
+    });
+
+    return {
+        gameInit
+    };
+
+})();
