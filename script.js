be36ff254686f67d7c245a558611dfbd10c3a0f2
Stu Pender <stu1992@gmail.com>
2021-04-26 10:48:20 -0500

    players&board

--- script.js
+++ script.js
@@ -1,29 +1,74 @@
 // Holds the gameboard array -- using modules
-let gameBoard = {
-    gameboard: [X, X, X, O, O, O, X, X, X],
-};
+let gameBoardModule = (() => {
+    let boardArray = ["", "", "", "", "", "", "", "", ""];
+    let gameBoard = document.querySelector(".board");
+    let cells = Array.from(document.querySelectorAll(".cell"));
+    let winner = null;
+
+    let render = () => {
+        boardArray.forEach((mark, index) => {
+            cells[index].textContent = boardArray[index];
+        })
+    }
+
+    let reset = () => {
+        boardArray = ["", "", "", "", "", "", "", "", ""];
+    }
+
+    let checkWin = () => {
+        let winArrays = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        // Look over this for a stronger understanding
+        winArrays.forEach((combo) => {
+            if (boardArray[combo[0]]
+            && boardArray[combo[0]] === boardArray[combo[1]]
+            && boardArray[combo[0]] === boardArray[combo[2]]) {
+                winner = "current";
+            }
+        });
+        return winner || (boardArray.includes("") ? null : "Tie");
+    };
+
+    return {
+        render,
+        gameBoard,
+        cells,
+        boardArray,
+        checkWin,
+        reset
+    }
+})();
 
 // Controls the flow of the game -- using modules
-let displayController = {
-    init: function() {
-    },
-    cacheDOM: function() {
-    },
-    bindEvents: function() {
-    },
-    render: function() {
-    }
+let gamePlay = {
+    
 };
 
 // Holds the players -- using factories
-let player = (selection) => {
-    let currentPlayer = selection;
-    return {
-        currentPlayer: currentPlayer
+let playerFactory = (name, selection) => {
+    let playTurn = (board, cell) => {
+        let index = board.box.findIndex(position => position === cell);
+        if (board.boardArray[index] = "") {
+            board.render();
+            return index;
+        }
+        return null
     };
-}
-
-let playerOne = new Player();
+    return {
+        name,
+        mark,
+        playTurn
+    }
+};
 
 // REVEALING MODULE PATTERN
 // let displayController = (function(){
